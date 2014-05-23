8428c31287037aa36a9082a9db914a53b9288d09
Diego Sapriza <diego@sapriza.net>
2014-05-23 09:55:41 -0300

    configuracion

--- module/GithubConnector/config/module.config.php
+++ module/GithubConnector/config/module.config.php
@@ -5,7 +5,33 @@ return array(
         'service-mapper' => [
             'github' => 'GithubService'
         ]
-    ],  
+    ],
+    'github-connector' => [
+        /**
+         * Account name user or team
+         */
+        'account-name' => '',
+        /**
+         * Default filters for issues
+         */
+        'issue-filters' => [ // How to filter issues
+            'state' => 'open'
+        ],
+        /**
+         * User to use for connection
+         */
+        'user_connect' => [
+            'username' => '',
+            'password' => ''
+        ],
+        /**
+         * Application OAuth if no user provided
+         */
+        'oauth' => [
+            'oauth_consumer_key' => '',
+            'oauth_consumer_secret' => ''
+        ]
+    ],
     'service_manager' => [
         'factories' => [
             'GithubConnector\GithubService' => function ($sm)
