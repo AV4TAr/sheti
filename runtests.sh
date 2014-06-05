19c8d353107b781bb4cc0784da2f42c74923dbfc
Diego Sapriza <diego@sapriza.net>
2014-06-05 11:35:55 -0300

    phpcs y tests

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-for FILE in module/*/tests/phpunit.xml;
+./vendor/bin/php-cs-fixer -v fix ./module --dry-run --config-file ./.php_cs
+
+for FILE in module/*/test/phpunit.xml;
 do
     DIR=$(dirname ${FILE});
     cd $DIR
