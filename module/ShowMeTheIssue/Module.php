577ca70b1896b337eb19ccdc85611549159c95fa
Diego Sapriza <diego@sapriza.net>
2014-06-20 10:31:37 -0300

    phpci

--- module/ShowMeTheIssue/Module.php
+++ module/ShowMeTheIssue/Module.php
@@ -32,9 +32,7 @@ class Module implements AutoloaderProviderInterface
                 __DIR__ . '/autoload_classmap.php',
             ),
             'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-            // if we're in a namespace deeper than one level we need to fix the \ in the path
-                    __NAMESPACE__ => __DIR__ . '/src/' . str_replace('\\', '/' , __NAMESPACE__),
+                'namespaces' => array( __NAMESPACE__ => __DIR__ . '/src/' . str_replace('\\', '/' , __NAMESPACE__),
                 ),
             ),
         );
